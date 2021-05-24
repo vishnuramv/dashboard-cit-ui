0952d562c3c2a0bdc34f1a2ea80e7e6d85a8890d
Sudharsan <sudharsan.0125@gmail.com>
2021-05-24 19:03:58 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import Reset from "./Components/Reset/Reset";
+import Signup from "./Components/Signup/Signup";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/" exact>
-            <h1>Welcome!</h1>
+          <Route path="/signup">
+            <Signup />
           </Route>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/reset">
             <Reset />
+          </Route>
+          <Route path="/" exact>
+            <h1>Welcome!</h1>
+            <Link to="/login">Login</Link>
           </Route>
         </Switch>
       </div>
