44c67c7872c75f68c5a08b185568a6e46f595f01
Sudharsan <sudharsan.0125@gmail.com>
2021-05-24 19:04:29 +0530

    Completed Login Page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import EmailIcon from "@material-ui/icons/Email";
 import LockIcon from "@material-ui/icons/Lock";
-import { Button, Card, CardActions, CardContent } from "@material-ui/core";
+import { Error as ErrorIcon } from "@material-ui/icons";
+import { Card, CardActions, CardContent } from "@material-ui/core";
 import "./Login.css";
 
 const Login = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsge, setErrorMsge] = useState(null);
+
+  const emailChange = (event) => {
+    setErrorMsge(null);
+    setEmail(event.target.value);
+  };
+
+  const passwordChange = (event) => {
+    setErrorMsge(null);
+    setPassword(event.target.value);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+    if ((email && password) !== "") {
+      history.push("/");
+    } else {
+      setErrorMsge("The Fields cannot be Empty!");
+    }
+  };
+
   return (
     <div className="login">
       <div className="login__container">
@@ -16,19 +42,39 @@ const Login = () => {
           />
           <CardContent>
             <form className="login__form">
+              <h2>Sign In</h2>
+              {errorMsge ? (
+                <p>
+                  <ErrorIcon /> {errorMsge}
+                </p>
+              ) : null}
               <div>
                 <label for="email">
                   <EmailIcon />
                 </label>
-                <input type="text" value="" name="email" id="email" />
+                <input
+                  type="text"
+                  value={email}
+                  name="email"
+                  id="email"
+                  onChange={emailChange}
+                  placeholder="youremail@example.com"
+                />
               </div>
               <div>
                 <label for="password">
                   <LockIcon />
                 </label>
-                <input type="password" value="" id="password" name="password" />
+                <input
+                  type="password"
+                  value={password}
+                  id="password"
+                  name="password"
+                  onChange={passwordChange}
+                  placeholder="password"
+                />
               </div>
-              <button className="login__btn" type="submit">
+              <button className="login__btn" type="submit" onClick={signIn}>
                 Sign in
               </button>
             </form>
@@ -38,11 +84,11 @@ const Login = () => {
             <div className="login__actions">
               <div className="login__redirects">
                 <div>
-                  <p>Not a user??</p>
-                  <a href="/signup">Create an account</a>
+                  <p>Not a user?</p>
+                  <Link to="/signUp">Create an account</Link>
                 </div>
                 <div>
-                  <a href="/reset">Forgot password</a>
+                  <Link to="/reset">Forgot password</Link>
                 </div>
               </div>
             </div>
